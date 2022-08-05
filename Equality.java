e2625369cffdfdb1533cb1ab6fc802af4712ec3f
Altaf Ali <jeeshan90ali57@gmail.com>
2022-08-05 12:34:08 +0530

    Add files via upload

---
+++ Equality.java
@@ -0,0 +1,15 @@
+package assingment;
+//EQUALITY OF TWO ARRAY
+
+import java.util.Arrays;
+
+public class equality {
+	public static void main(String[] args) {
+		int [] arr1= {1,2,3,4};
+		int [] arr2 ={1,2,3,4};
+		
+		System.out.println("arr1 is equal to arr2 "+ Arrays.equals(arr1,arr2));
+
+	}
+
+}
