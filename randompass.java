212a491d96c0a06df3fa038432e98f368cb4b5ce
Altaf Ali <jeeshan90ali57@gmail.com>
2022-09-29 17:59:28 +0530

    Add files via upload

---
+++ randompass.java
@@ -0,0 +1,20 @@
+package test;
+	import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+    class Main{
+	        public static void main(String a[]){
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("Generate your Security Code");
+	        String code = sc.next();
+	        String regex = "[A-Za-z0-9@*#]{8}";
+	        Pattern p = Pattern.compile(regex);
+	        Matcher m = p.matcher(code);
+	        boolean matchFound = m.find();
+	        if(matchFound)
+	        System.out.println("Security Code Generated Successfully");
+	        else
+	        System.out.println("Invalid Security Code, Try Again!");
+	    }
+	}
+	
